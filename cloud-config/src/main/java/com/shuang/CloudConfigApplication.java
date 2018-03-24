22f46f7dd511af5dab6e2a57ad4783bd6ba3bc5b
wzshuang <443509543@qq.com>
2018-03-24 23:57:01 +0800

    update

--- cloud-config/src/main/java/com/shuang/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/shuang/CloudConfigApplication.java
@@ -1,22 +1,14 @@
 package com.shuang;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@Configuration
-@EnableAutoConfiguration
-@EnableDiscoveryClient
 @EnableConfigServer
-@EnableEurekaClient
 public class CloudConfigApplication {
 
 	@RequestMapping("/")
